a886265dd0410faaa8d461a68201e5e1fb446b20
MaggieSaurusRex <perfection_in_progress@live.de>
2022-03-20 11:07:11 +0100

    project creation

---
+++ ballantion/src/main/java/de/bubmlebat/ballantion/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package de.bubmlebat.ballantion;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    
+}
